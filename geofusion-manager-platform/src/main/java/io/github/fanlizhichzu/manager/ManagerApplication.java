4c2beb999ddc6db0dfe04ddfffd9eb417f6aa9f0
fanlz <lizh.fan@outlook.com>
2025-07-17 17:14:14 +0800

    新增数据库和flyway

--- geofusion-manager-platform/src/main/java/io/github/fanlizhichzu/manager/ManagerApplication.java
+++ geofusion-manager-platform/src/main/java/io/github/fanlizhichzu/manager/ManagerApplication.java
@@ -2,6 +2,8 @@ package io.github.fanlizhichzu.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(scanBasePackages = "io.github.fanlizhichzu.*")
 public class ManagerApplication {
